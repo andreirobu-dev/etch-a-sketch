91debe5e98a25e9bc45736980e47c3953d43b3fd
andreirobu-dev <dev.andreirobu@pm.me>
2026-03-25 11:10:53 +0200

    Add hover effect for cells

--- script.js
+++ script.js
@@ -8,12 +8,26 @@ function fillGrid (cellsPerRow) {
     const cellSize = 100 / cellsPerRow;
 
     for (let i=0; i<cells; i++) {
-        const square = document.createElement("div");
-        square.classList.toggle("square");
-        square.style.width = `${cellSize}%`;
-        grid.appendChild(square);
+        const cell = document.createElement("div");
+        cell.classList.toggle("cell");
+        cell.style.width = `${cellSize}%`;
+        grid.appendChild(cell);
     }
+}
+
+function changeCellClass (event) {
+    event.target.classList.toggle("cell");
+    event.target.classList.toggle("cell-hover");
+}
+
+function changeColorOnHover (cells) {
+    cells.forEach ((cell) => { 
+        cell.addEventListener("mouseover", changeCellClass)
+    })
 }
 
 fillGrid(cellsPerRow);
 body.appendChild(grid);
+const cells = document.querySelectorAll(".cell");
+console.log(cells.length);
+changeColorOnHover(cells);
